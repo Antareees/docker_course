bfc2c9303f719c09e596d468665df8d4b2d10f97
Antareees <efimm2004@gmail.com>
2024-11-10 17:47:57 +0400

    resolved alembic error

--- src/db_manager/db_manager.py
+++ src/db_manager/db_manager.py
@@ -5,16 +5,12 @@ from schemas.models import *
 
 class DBManager:
     def __init__(self, user, password) -> None:
-        '''создание базы данных postgres_db если ее нет'''
         self.user = user
         self.__password = password
         self.engine = create_engine("postgresql+psycopg2://postgres:postgres@postgres:5432/postgres")
         self.session = Session(bind=self.engine)
 
     def get_programs(self) -> list:
-        '''метод выдает записи компаний с количеством их вакансий'''
-
         session = self.session
-        select_result = None
         select_result = session.query(Programs).all()
         return select_result
